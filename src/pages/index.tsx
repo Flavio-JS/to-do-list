65cb8f689ea4a2f740d7ce6d99d3173399994f12
Flavio Filho <flavio.filho@trinusco.com.br>
2023-02-07 23:39:24 -0300

    finalizado header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
+import PlusCircle from "@/components/Icons/PlusCircle";
 import { NextPage } from "next";
-import { Header, PaginaInicial } from "./style";
+import { Header, PaginaInicial, HeaderButton } from "./style";
 
 const HomePage: NextPage = () => {
   return (
     <PaginaInicial>
       <Header>
         <h1>TO DO | YOUR LISTS</h1>
+        <HeaderButton startIcon={<PlusCircle />} variant="text">
+          Add new List
+        </HeaderButton>
       </Header>
     </PaginaInicial>
   );
